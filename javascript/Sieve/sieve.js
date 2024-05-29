621e85e14630fcd3a005c16bfcf213213ebba114
James Moore <jameswmoore99@gmail.com>
2024-05-29 13:46:44 -0600

    ~ 7 of 8 tests passing, 8 wont pass due to how slow my solution is, brute force solution done in JS

--- javascript/Sieve/sieve.js
+++ javascript/Sieve/sieve.js
@@ -1,6 +1,35 @@
 class Sieve {
   NthPrime(n) {
-    throw new Error("unimplemented");
+    console.log(n);
+    let primes = [2, 3, 5, 7];
+    // Early return for first 4 primes
+    if (n < primes.length) return primes[n];
+
+    let number = 9;
+
+    while (primes.length !== n + 1) {
+      // Check division of the primes
+      if (!this.CheckDivOfPrimes(number, primes)) {
+        primes.push(number);
+      }
+
+      // Only check odds as evens are never prime after 2
+      number += 2;
+    }
+
+    return primes[n];
+  }
+
+  CheckDivOfPrimes(n, primes) {
+    // Only need to check up to the square root of n
+    let limit = Math.ceil(Math.sqrt(n));
+    for (let i = 0; i < limit; i++) {
+      if (n % primes[i] === 0) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
 
